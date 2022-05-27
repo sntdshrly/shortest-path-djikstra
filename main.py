7e22bdd7371293e1e054ebf67f743ee5a90fafec
Arya Tri Putra Majiah <71741042+aryatripm@users.noreply.github.com>
2022-05-27 11:11:51 +0700

    lintasan terpendek

--- main.py
+++ main.py
@@ -17,18 +17,28 @@ def main():
     # simpul a = 0
 
     awal = 0
-    tujuan = 6
+    akhir = 11
 
     # simpul_dipilih = dijkstra(awal,tujuan,graf)
     simpul_dipilih = dijkstra_2(graf,awal)
 
-    for tujuan, simpul in enumerate(simpul_dipilih):
-        print(f"Simpul Tujuan : {tujuan}\tLintasan Terakhir : {simpul[0]}\tJarak : {simpul[1]}")
+    
+    def lintasan(simpul, graf_hasil):
+        if simpul[1] == None:
+            return ' Selesai'
+        print('<-', end='')
+        print(simpul[1], end='')
+        return lintasan(graf_hasil[simpul[1]], graf_hasil)
 
-    # print(f'start/end nodes: {awal} -> {tujuan}')
-    # simpul_dipilih = list(simpul_dipilih)
-    # print(f'shortest path: {[lst[0] for lst in simpul_dipilih]}')
-    # print(f'total distance: {sum([lst[2] for lst in simpul_dipilih])}')
+    print(f'start/end nodes: {awal} -> {akhir}')
+    print('Tabel Hasil :')
+    for tujuan, simpul in enumerate(simpul_dipilih):
+        print(f"Simpul Tujuan : {tujuan}\tLintasan Terakhir : {simpul[1]}\tJarak : {simpul[0]}")
+
+    print('shortest path:')
+    print(akhir, end='')
+    print(lintasan(simpul_dipilih[akhir], simpul_dipilih))
+    print(f'total distance: {simpul_dipilih[akhir][0]}')
 
 
 if __name__ == '__main__':
